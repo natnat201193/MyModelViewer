8bfadfba492d6484293fed281ebd58edc0ca36eb
natnat <33712044+natnat201193@users.noreply.github.com>
2021-11-05 01:44:57 +0000

    Add project files.

---
+++ MyModelViewer/MyModelViewer.cpp
@@ -0,0 +1,12 @@
+﻿// MyModelViewer.cpp : Defines the entry point for the application.
+//
+
+#include "MyModelViewer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
